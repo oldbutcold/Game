cb42530de9bf00d9d32fc748df50bc3446768e8e
Aleksey <a.e.sokolkov@gmail.com>
2021-04-16 11:48:31 +0500

    Добавил передвижение и границы экрана

---
+++ Assets/Scripts/Boundaries.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+    
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,
+        Screen.height, Camera.main.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+    
+    void Update()
+    {
+        var viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, -screenBounds.x + objectWidth,
+            screenBounds.x - objectWidth);
+
+        viewPos.y = Mathf.Clamp(viewPos.y, -screenBounds.y + objectHeight,
+            screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var moveInput = new Vector2(Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"));
+        rb.MovePosition(rb.position + moveInput.normalized * (speed * Time.fixedDeltaTime));
+    }
+}
