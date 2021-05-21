f173fb8e304c640a33ed4f4aeac499376d88bcab
Aleksey <a.e.sokolkov@gmail.com>
2021-05-21 17:06:50 +0500

    Iteractable, Canvas, SceneTeleport

--- Assets/Scripts/Objects/Lever.cs
+++ Assets/Scripts/Objects/Lever.cs
@@ -1,36 +1,27 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Lever : Interactable
 {
     public bool leverActive;
     public Sprite pushedLeverSprite;
     private SpriteRenderer mySprite;
-    private GameObject player;
-    private GameObject lever;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
-        lever = GameObject.FindWithTag("Lever");
         mySprite = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         var sprite = GetComponent<SpriteRenderer>();
-        sprite.sortingOrder = Mathf.RoundToInt(transform.position.y * -10f);
-        if (player is null || lever is null) return;
-        if (Input.GetKeyDown(KeyCode.E) && Vector3.Distance(player.transform.position, lever.transform.position) < 1)
+        sprite.sortingOrder = Mathf.RoundToInt(transform.position.y * -10f - 1);
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
         {
             leverActive = !leverActive;
             var x = mySprite.sprite;
             mySprite.sprite = pushedLeverSprite;
             pushedLeverSprite = x;
         }
-        
     }
 }
---
+++ Assets/Scripts/Objects/SceneTeleport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTeleport : Interactable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+    }
+}
