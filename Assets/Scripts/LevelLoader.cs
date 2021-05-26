4fdeede68dae85ea825bfc0759cbc92adc291d7b
Mikhail Lanets <theoilside@gmail.com>
2021-05-27 01:35:41 +0500

    Options в меню + Почти Transitions между сценами

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (false)
+            LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
