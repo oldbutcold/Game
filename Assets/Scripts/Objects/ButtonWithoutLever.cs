470b4eb51b674d4b5f0faeea100e985a66cf7a6a
Aleksey <a.e.sokolkov@gmail.com>
2021-05-28 04:41:24 +0500

    3 уровень

---
+++ Assets/Scripts/Objects/ButtonWithoutLever.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonWithoutLever : MainScript
+{
+    public bool buttonActive;
+    public Sprite pushedButtonSprite;
+    private SpriteRenderer mySprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+        buttonActive = false;
+    }
+
+    public void ActivateButton()
+    {
+        buttonActive = true;
+        PushedButtons++;
+        mySprite.sprite = pushedButtonSprite;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Rigidbody2D>() == null) return;
+        if (buttonActive) 
+            return;
+        ActivateButton();
+    }
+}
--- Assets/Scripts/Objects/MainScript.cs
+++ Assets/Scripts/Objects/MainScript.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonWithourLever : MonoBehaviour
+public class MainScript : MonoBehaviour
 {
+    public static int PushedButtons = 0;
     // Start is called before the first frame update
     void Start()
     {
